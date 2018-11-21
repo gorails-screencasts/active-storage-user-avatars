8ea166b4c9e023043bffe99da5a7311819b01777
Chris Oliver <excid3@gmail.com>
2018-11-21 08:42:38 -0600

    User avatars with Devise and Rails ActiveStorage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+
+  def user_avatar(user, size=40)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      gravatar_image_url(user.email, size: size)
+    end
+  end
 end
